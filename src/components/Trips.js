fa2ad1827909fb6b0061371e1fe5ee3aaeb70e11
Sanya <kasaudhansanya@gmail.com>
2020-11-17 22:08:44 +0530

    img

--- src/components/Trips.js
+++ src/components/Trips.js
@@ -1,14 +1,52 @@
 import React from 'react'
 import styled from 'styled-components'
+import Img from 'gatsby-image'
 import {useStaticQuery,graphql} from 'gatsby'
+import { Button } from './Button'
+import {ImLocation} from 'react-icons/im'
 const Trips = () => {
-    return (
+   
         const data=useStaticQuery(graphql
             `query TripsQuery{
                 allTripsJson{
-                    
+                    edges{
+                        node{
+                            alt
+                            button
+                            name
+                            img{
+                                childImageSharp{
+                                    fluid{
+                                        ...GatsbyImageSharpFluid
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }`)
+
+            function getTrips(data){
+                const tripsArray=[]
+                data.allTripsJson.edges.forEach((item,index) =>{
+                    tripsArray.push(
+                    <ProductCard>
+                        cdcd
+                        <ProductImg src={item.node.img.childElementSharp.fluid.src}
+                        fluid={item.node.img.childElementSharp.fluid} />
+                        <ProductInfo>
+                            <TextWrap>
+                                <ImLocation />
+                                <ProductTitle>{item.node.name}</ProductTitle>
+                            </TextWrap>
+                            <Button to="/trips">{item.node.button}</Button>
+                        </ProductInfo>
+                    </ProductCard>)
+
+                })
+                return tripsArray
+            }
+            return (
             <ProductionContainer>
                 <ProductsHeading>Heading</ProductsHeading>
                 <ProductWrapper>Wrapper</ProductWrapper>
@@ -36,6 +74,45 @@ color:#000;
 `
 
 const ProductWrapper=styled.div
+`
+display:grid;
+grid-template-columns:repeat(4,1fr);
+grid-gap:10px;
+justify-items:center;
+padding:0 2rem;
+
+@media screen and (max-width:1200px){
+    
+grid-template-columns:1fr 1fr;
+}
+@media screen and (max-width:868px){
+    
+    grid-template-columns:1fr;
+    }
+`
+
+const ProductCard=styled.div
+`
+line-height:2;
+width:100%;
+height:500px;
+position:relative;
+border-radius:10px;
+transition:1.2s ease;
+
+`
+
+const ProductInfo= styled.div
+`
+
+`
+
+const TextWrap=styled.div
+`
+
+`
+
+const ProductTitle=styled.div
 `
 
 `
